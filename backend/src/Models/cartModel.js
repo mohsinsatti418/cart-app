7463c34c2600cad3581e205c634e9d359005d563
Mohsin <sattimohsin418@gmail.com>
2026-03-19 14:06:13 +0500

    frontend only - removed backend

--- backend/src/Models/cartModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const cartItemSchema = new mongoose.Schema({
-  name:     { type: String, required: true },
-  price:    { type: Number, required: true },
-  quantity: { type: Number, default: 1 },
-}, { timestamps: true });
-
-export default mongoose.model("Cart", cartItemSchema);
